7b4c91543137c23076a52ab4b1137f332f4fbbce
kdb11 <kevin.badwi@medieinstitutet.se>
2023-03-08 04:19:03 +0100

    calc

--- 1-basics/src/Components/Calculate.jsx
+++ 1-basics/src/Components/Calculate.jsx
@@ -1,13 +1,22 @@
 export function Calculate() {
+    
+    function AddNumbers() {
+        let x = document.getElementById("x").value;
+        let y = document.getElementById("y").value;
+/*         let calc = document.getElementById("calc"); */
+        let z = document.getElementById("result");
+        z.innerHTML = JSON.parse(x) + JSON.parse(y);  
+    }
+    
     return <div>
         <form>
         <p>Add two numbers</p>
-        <input type="text" />
+        <input id="x" type="number"/>
         <p>+</p>
-        <input type="text"/>
+        <input id="y" type="number"/>
         <br />
-        <button id="=">=</button>
-        <p id="result">Result</p>
+        <button id="calc" onClick={AddNumbers}>=</button>
+        <p id="result"></p>
         </form>
     </div>
 }
