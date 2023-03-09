91684eb2ec9eef675b5566421aa80e73be806f86
kdb11 <kevin.badwi@medieinstitutet.se>
2023-03-09 01:21:40 +0100

    placeholder

--- 1-basics/src/Components/Products.jsx
+++ 1-basics/src/Components/Products.jsx
@@ -1,5 +1,21 @@
 export function Products() {
-    const MyProducts = [{ }, { }, { }];
+    const MyProducts = [
+        {
+            brand:'Mircidec', 
+            description:'virri najs, fast etc',
+            price: 'for you my friend good price!'
+        }, 
+        {
+            brand:'Bi m double v', 
+            description:'strong horse power',
+            price: 'tell me best price?'
+        }, 
+        {
+            brand:'Aoudi', 
+            description:'2025 limited edition, gold plated',
+            price: '1.9 million dirham'
+        }, 
+    ];
 
     const Html = MyProducts.map((MyProduct) => {
         return (<div key={MyProduct.id}>
