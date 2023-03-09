ec9ddfbe6d80fba0de4c55a21d2f888370ce1cf0
kdb11 <kevin.badwi@medieinstitutet.se>
2023-03-09 01:12:55 +0100

    products list with errormsg
    Warning: Each child in a list should have a unique "key" prop.

--- 1-basics/src/Components/Products.jsx
+++ 1-basics/src/Components/Products.jsx
@@ -1,5 +1,15 @@
 export function Products() {
-    return <div>
-        <p>Products</p>
-    </div>
+    const MyProducts = [{ }, { }, { }];
+
+    const Html = MyProducts.map((MyProduct) => {
+        return (<div key={MyProduct.id}>
+        <p>{MyProduct.brand}</p>
+        <p>{MyProduct.description}</p>
+        <p>{MyProduct.price}</p>
+    </div>)
+    });
+
+    return (<>
+        {Html}
+    </>);
 }
