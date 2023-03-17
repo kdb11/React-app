c26f5e0ca7c8e05583e4f716989058c076e22f17
kdb11 <kevin.badwi@medieinstitutet.se>
2023-03-17 18:23:08 +0100

    form

--- 1-basics/src/Components/Fomula.jsx
+++ 1-basics/src/Components/Fomula.jsx
@@ -4,19 +4,34 @@ import { Adress } from './models/Adress.js';
 
 export function Formula() {
 
-    let [name, setName] = useState("0");
+    const [user, setUser] = useState(new User("", "", "", new Adress("", "", "")));
+
+    const HandleChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value})
+    }
+
+    const HandleAdressChange = (e) => {
+        setUser({ ...user, adress: {...user.adress, [e.target.name]: e.target.value }})
+    };
+
+/*     let [name, setName] = useState("0");
     let [email, setEmail] = useState("0");
-    let [adress, setAdress] = useState("0");
+    let [adress, setAdress] = useState("0"); */
 
     return <div>
         <p>formula</p>
-        <p> {} </p>
         <form action="">
-            <input type="text" placeholder='name'/>
-            
-            <input type="text" placeholder='email' />
+            <input type="text" placeholder='name' value={user.name} onChange={HandleChange} name="name" />
+            <p> {JSON.stringify(user)} </p>
+            <input type="text" placeholder='email' value={user.email} onChange={HandleChange} name="email"/>
             <p> </p>
-            <input type="text" placeholder='adress' />
+            <input type="text" placeholder='adress' value={user.adress} onChange={HandleChange} name="adress"/>
+            
+            <input type="text" placeholder='street' value={user.adress.street} onChange={HandleAdressChange} name="street" />
+            <p></p>
+            <input type="text" placeholder='zip' value={user.adress.zip} onChange={HandleAdressChange} name="zip"/>
+            <p> </p>
+            <input type="text" placeholder='city' value={user.adress.city} onChange={HandleAdressChange} name="city"/>
         </form>
 
     </div>
