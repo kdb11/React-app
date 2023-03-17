27bd3ba5b004182998117af3b7368215680f847c
kdb11 <kevin.badwi@medieinstitutet.se>
2023-03-17 18:36:32 +0100

    error with adress

--- 1-basics/src/Components/Fomula.jsx
+++ 1-basics/src/Components/Fomula.jsx
@@ -14,6 +14,10 @@ export function Formula() {
         setUser({ ...user, adress: {...user.adress, [e.target.name]: e.target.value }})
     };
 
+    const SaveUser = (e) => {
+        console.log("User:", user)
+    }
+
 /*     let [name, setName] = useState("0");
     let [email, setEmail] = useState("0");
     let [adress, setAdress] = useState("0"); */
@@ -27,11 +31,14 @@ export function Formula() {
             <p> </p>
             <input type="text" placeholder='adress' value={user.adress} onChange={HandleChange} name="adress"/>
             
+            <div>
             <input type="text" placeholder='street' value={user.adress.street} onChange={HandleAdressChange} name="street" />
             <p></p>
             <input type="text" placeholder='zip' value={user.adress.zip} onChange={HandleAdressChange} name="zip"/>
             <p> </p>
             <input type="text" placeholder='city' value={user.adress.city} onChange={HandleAdressChange} name="city"/>
+            </div>
+            <button type='button' onClick={SaveUser}>Save</button>
         </form>
 
     </div>
