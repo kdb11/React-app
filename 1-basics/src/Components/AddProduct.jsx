e6296d03ae33fd74f801817a498e5477dd5832d2
kdb11 <kevin.badwi@medieinstitutet.se>
2023-03-20 02:50:21 +0100

    validation with react hook form

--- 1-basics/src/Components/AddProduct.jsx
+++ 1-basics/src/Components/AddProduct.jsx
@@ -1,10 +1,18 @@
 import { Item } from "./models/Item";
 import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 export function AddProduct({ addProduct }) {
     const[product, SetProduct] = useState(new Item("", "", ""));
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    
+    const onSubmit = (data, e) => {
+        e.preventDefault();
+        console.log("product", product);
+        addProduct(product);
+        console.log(data); }
 
-    const HandleChange = (e) => {
+    const handleChange = (e) => {
         
         if (e.target.type === "text") { 
             SetProduct({...product, [e.target.name]: e.target.value});
@@ -15,19 +23,13 @@ export function AddProduct({ addProduct }) {
         }
     };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log("product", product);
-        addProduct(product);
-    }
-
-
     return <div>
-        <form onSubmit={handleSubmit}>
-        <input type="text" value={product.brand} placeholder="Brand" onChange={HandleChange} name="brand"/>
-        <input type="text" value={product.description} placeholder="Description" onChange={HandleChange} name="description"/>
-        <input type="number" value={product.price} placeholder="Price" onChange={HandleChange} name="price"/>
-        <button type="submit">Save item</button>
+        <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" {...register("brand", { required: true, minLength: 3 })} value={product.brand} placeholder="Brand" onChange={handleChange} name="brand" />
+        <>{errors.brand && (<span>should be at least 3 charachters</span>)}</>
+        <input type="text" value={product.description} placeholder="Description" onChange={handleChange} name="description"/>
+        <input type="number" value={product.price} placeholder="Price" onChange={handleChange} name="price"/>
+        <input type="submit" value="Save item" />
         </form>
     </div>
 }
